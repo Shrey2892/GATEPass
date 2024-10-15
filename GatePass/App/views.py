e96ae6ebced2d97894b3316ecbbee643e8dedf5f
shrey2892 <nairshreya227@gmail.com>
2024-10-15 13:06:33 +0530

    Login and Register Page Updated.

--- GatePass/App/views.py
+++ GatePass/App/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def register(request):
+    #  template = loader.get_template('pages-register.html')
+    #  return HttpResponse(template.render())
+    return render (request,'pages-register.html')
+
+
+def login(request):
+    #  template = loader.get_template('pages-login.html')
+    #  return HttpResponse(template.render())
+    return render(request,'pages-login.html')
+
